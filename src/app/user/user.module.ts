34b2389e47e48b14294981055d165c0f2addd776
Mostafa-Rabea <mostafa.rapi3@gmail.com>
2023-09-24 02:18:13 +0300

    added

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartComponent } from './cart/cart.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SellerComponent } from './seller/seller.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    CartComponent,
+    ProfileComponent,
+    SellerComponent
+  ],
+  imports: [
+    CommonModule, RouterModule
+  ],
+  exports:[
+    CartComponent,
+    ProfileComponent,
+    SellerComponent
+  ]
+})
+export class UserModule { }
